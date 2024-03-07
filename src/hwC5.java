6652b608435d6df6d5f60932caa5fa0927d45c65
Михаил <@zaymi0>
2024-03-07 14:30:50 +0300

    Update

--- src/hwC5.java
+++ src/hwC5.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class hwC5 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String str = scanner.nextLine();
 
+        for (int i = 0; i <= str.length(); i++) {
+            char ch = str.charAt(i);
+            System.out.println(ch);
+        }
     }
 }
