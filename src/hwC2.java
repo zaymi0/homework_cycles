39bf251d45a82ddcde6f3129833db29433bbb747
Михаил <@zaymi0>
2024-03-07 13:50:04 +0300

    Update

--- src/hwC2.java
+++ src/hwC2.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class hwC2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int num = scanner.nextInt();
 
+        for (int i = 0; i < num; i++){
+
+            if (i % 2 == 0){
+                System.out.print(i + " ");
+            }
+        }
     }
 }
