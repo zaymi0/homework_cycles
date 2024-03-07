78f58544d918679ca008bd21a870cfd22cb133ca
Михаил <@zaymi0>
2024-03-07 14:22:18 +0300

    Update

--- src/hwC4.java
+++ src/hwC4.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class hwC4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int answer = 1;
+        System.out.print("Введите число: ");
+        int num = scanner.nextInt();
 
+        for (int i = 1; i <= num; i++){
+            answer = answer * i;
+        }
+
+        System.out.println(answer);
     }
 }
