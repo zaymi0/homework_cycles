047d3f6bcc9d3fc5d9b95a302f97baa94e604106
Михаил <@zaymi0>
2024-03-07 14:09:26 +0300

    Update

--- src/hwC3.java
+++ src/hwC3.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class hwC3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        int num1 = 1;
+        int sum;
+        System.out.print("Введите число: ");
+        int num3 = scanner.nextInt();
 
+        for (int i = 2; i < num3; i++){
+            sum = num + num1;
+            num = num1;
+            num1 = sum;
+        }
+
+        System.out.println(num1);
     }
 }
